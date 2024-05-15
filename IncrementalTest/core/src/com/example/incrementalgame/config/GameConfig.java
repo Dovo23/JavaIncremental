756e0ed371a0472bced0b52c4bc1c7fd446c3a3c
Paul März <88943663+Dovo23@users.noreply.github.com>
2024-05-15 12:02:25 +0200

    Delete IncrementalTest directory

--- IncrementalTest/core/src/com/example/incrementalgame/config/GameConfig.java
+++
@@ -1,8 +0,0 @@
-package com.example.incrementalgame.config;
-
-public interface GameConfig {
-    float WORLD_WIDTH = 800;
-    float WORLD_HEIGHT = 480;
-    float BUTTON_WIDTH = 150;
-    float BUTTON_HEIGHT = 50;
-}
